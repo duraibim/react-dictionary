15de8225d7aba96ecd2ae1589017f3ba2c0b5012
Manal <duraibimn@gmail.com>
2021-06-29 16:41:44 -0500

    added search engine component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import images from "./images.gif";
+import Dictionary from "./Dictionary";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={images} className="images img-fluid" alt="images"/>     
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={images} className="images img-fluid" alt="images"/>     
+        </header>
+
+        <Dictionary />
+
+        <footer className="App-footer">Coded by Manal</footer>
+      </div>
     </div>
   );
 }
