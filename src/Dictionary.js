a920e709417473ccfb9a8e7f50b35acce0162589
Manal <duraibimn@gmail.com>
2021-06-30 16:54:41 -0500

    added axios

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,21 @@
 import React, {useState} from "react";
+import axios from "axios";
 import './Dictionary.css';
+
+
+function handleResponse(response){
+    console.log(response);
+}
 
 export default function Dictionary(){
     let [keyword, setKeyword] = useState("");
     function search(event){
         event.preventDefault();
-        alert(`Searching for ${keyword}`);
+
+        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+        axios.get(apiUrl).then(handleResponse);
     }
+
 
     function handleKeywordChange(event){
         setKeyword(event.target.value);
