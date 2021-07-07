a479d87816d7887e9a3b713d93e5b8c04c15b76d
Manal <duraibimn@gmail.com>
2021-07-07 17:20:58 -0500

    edited photo component

--- src/Photos.js
+++ src/Photos.js
@@ -3,6 +3,7 @@ import "./Photos.css";
 
 export default function Photos(props){
     if (props.photos){
+        console.log(props.photos);
         return(
             <section className="Photos">
                 <div className="row">
@@ -10,12 +11,13 @@ export default function Photos(props){
                     return (
                         <div className="col-4" key={index}>
                             <a href={photo.src.original} target="_blank" rel="noreferrer">
-                            <img src={photo.src.tiny} className="img-fluid"/>
+                            <img src={photo.src.tiny} className="img-fluid" alt={photo.photographer}/>
                             </a>
                         </div>
                 );
                 })}
                 </div>
+
             </section>
         );
     } else {
