4afe6a4688e858f7df8ec73fdb9769b91eb61454
Manal <duraibimn@gmail.com>
2021-07-07 16:58:23 -0500

    added photo component

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -7,7 +7,7 @@ export default function Phonetics(props){
     return (
     
         <div className="Phonetics">
-            <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
+            <a href={props.phonetic.audio} target="_blank" rel="noreferrer" >
                 Listen
             </a> 
             {props.phonetic.text}
